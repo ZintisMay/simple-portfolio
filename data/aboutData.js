5249789ce8afc7122e2b12674ee282279d6037c9
DESKTOP-DRCSSPA\Zintis <zintismay@gmail.com>
2024-03-21 01:56:50 -0400

    about

---
+++ data/aboutData.js
@@ -0,0 +1,53 @@
+let aboutData = [
+  {
+    image: "zintisPorch.jpg",
+    title: "Who Am I?",
+    text: [
+      "My name is Zintis. It's a Latvian name, but I've lived in the US my whole life. Grew up in New Jersey, did college here at Montclair State before moving to Pennsylvania. In PA I spent a few years working in a casino as a poker dealer before deciding that wasn't for me and enrolling in the Rutgers Coding Bootcamp.",
+    ],
+  },
+  {
+    image: "PoemComber.png",
+    title: "Student",
+    text: [
+      "The Bootcamp gave me a really strong push into the world of coding. Prior to that I was studying some C++ on my own, but found that I didn't have any direction. By focusing on Web Development, I could learn the required technologies and techniques for making websites. Not only that, but learning the programming is a never-ending (and sometimes frustrating) learning experience.",
+    ],
+  },
+  {
+    image: "zintisHoodie.jpg",
+    title: "Junior Web Developer",
+    text: [
+      "My web dev career began as an intern at Arkadium NYC, a company that handles a plethora of online games and Microsoft's solitaire. I worked with their Inhabit Module, a tool that scrapes and dynamically generates quiz games from articles. In my time there I implemented two C# API's and a created a dynamic injectable quiz game.",
+    ],
+  },
+  {
+    image: "thorium2.png",
+    title: "Web Developer",
+    text: [
+      "Aside from doing some freelancing, my first full time position was with Thorium Digital in 2018. They are a small web dev firm that mostly creates marketing websites. I had my hands in all kinds of sites that you can see on my Portfolio page. I ended up wearing numerous hats, one of which was to help create their technical hiring process and onboard new hires.",
+    ],
+  },
+  {
+    image: "img_2443_360.jpg",
+    title: "Programming TA",
+    text: [
+      "In 2018 I found myself as a TA for the same Bootcamp I was in. I really enjoyed my time teaching and made a strong impact on my students. Being passionate, supportive, and exceedingly patient helps.",
+      "I wish I could keep doing it, but it eats up 5 hours every Saturday, which makes weekend plans difficult.",
+      "(I'm in blue in the back!)",
+    ],
+  },
+  {
+    image: "CP1.jpg",
+    title: "Game Designer",
+    text: [
+      "Around 2012 I took a casual interest in game design and started studying it. I also began learning programming thinking one day I would put them together and make a digital game. So far that hasn't been the case! I've enjoyed the fast iteration of paper prototypes, so that's what I've been doing!",
+    ],
+  },
+  {
+    image: "devx-screen-shot.png",
+    title: "Teacher, Tutor",
+    text: [
+      "Over the pandemic many people found out new hobbies, well I found a new job, teaching! I've worked with hundreds of students in classrooms and one-on-one. It's been really gratifying getting a chance to work with people who are just starting their programming journey, just like I was some 5-6 years ago.",
+    ],
+  },
+];
